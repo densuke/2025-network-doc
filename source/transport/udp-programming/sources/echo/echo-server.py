4378620d9c2631238a30fbdc58e8256095b97824
SATO Daisuke <densuke+github@gmail.com>
2025-06-18 08:50:51 +0900

    クライアント同様with文ベースに訂正

--- source/transport/udp-programming/sources/echo/echo-server.py
+++ source/transport/udp-programming/sources/echo/echo-server.py
@@ -3,22 +3,21 @@ import socket
 
 def main():
     # UDPソケットを作る
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        # 今回は、localhostのポート10000で待ち受けます(10000/udp)
+        server_address = ('localhost', 10000)
+        sock.bind(server_address)
 
-    # 今回は、localhostのポート10000で待ち受けます(10000/udp)
-    server_address = ('localhost', 10000)
-    sock.bind(server_address)
+        print(f'サーバーは {server_address} で待ち受けています')
 
-    print(f'サーバーは {server_address} で待ち受けています')
+        while True:
+            # データを受信する
+            data, address = sock.recvfrom(4096)
+            print(f'受信しました: {data} from {address}')
 
-    while True:
-        # データを受信する
-        data, address = sock.recvfrom(4096)
-        print(f'受信しました: {data} from {address}')
-
-        # 受け取ったデータをそのまま送り返す(エコー)
-        sent = sock.sendto(data, address)
-        print(f'送信しました: {data}')
+            # 受け取ったデータをそのまま送り返す(エコー)
+            sent = sock.sendto(data, address)
+            print(f'送信しました: {data}')
 
 if __name__ == '__main__':
     main()
